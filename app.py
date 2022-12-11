52ddc3125050562ac281b321167346f677aa0774
Yoga Fatwanto <95328326+Lulu-L2@users.noreply.github.com>
2022-12-11 16:13:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,26 +20,7 @@ def home():
 @app.route("/predict", methods = ["GET", "POST"])
 @cross_origin()
 def predict():
-    # df=pd.read_csv('deploy_df.csv')
-
-    # df.drop('Unnamed: 0',axis=1,inplace=True)
-
-    # x=df.drop('Price',axis=1)
-    # print(x.head())
-
-    # y=df['Price']
-
-    # #splitting the dataset
-    # from sklearn.model_selection import train_test_split
-    # X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 50)
-
-
-
-    # from lightgbm import LGBMRegressor
-
-    # clf=LGBMRegressor()
-    # clf.fit(X_train,y_train)
-
+    
     model=pickle.load(open('model.pkl','rb'))
   
     if request.method == "POST":
